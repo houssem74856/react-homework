29911ebae2890ec5c0d0688bc0d160358b1ac1eb
Houssem Mitiche <mitichehoussem@gmail.com>
2022-11-05 14:39:11 +0100

    homeworkreact

--- src/blogs.js
+++ src/blogs.js
@@ -1,22 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
+
+function reducer(blogs,action) {
+    switch (action.type) {
+        case 'new' :
+            return action.blogs
+    }
+}
 
 function Blogs() {
-    const [blogs,setBlogs] = useState([
-        {title : 'React', para : 'lorem ipsum...' , id : 1},
-        {title : 'Javascript Resources', para : 'lorem ipsum...' , id : 2},
-        {title : 'No New Features', para : 'lorem ipsum...' , id : 3},
-        {title : 'React', para : 'lorem ipsum...' , id : 4},
-    ])
+    const [blogs,dispatch] = useReducer(reducer,null)
+    const [isLoading,setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {fetch('http://localhost:2005/blogs')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            dispatch({type : 'new' , blogs : data})
+            setIsLoading(false)
+        })
+      },1000)
+    },[])
 
     return (
         <>
         <div className="blogs">
-            {blogs.map((blog) => (
+            { isLoading && <div>Loading...</div>}
+            { blogs && blogs.map((blog) => {
+                return (
+                <>
                 <div className="blog">
-                    <h1>{blog.title}</h1>
-                    <p>{blog.para}</p>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
                 </div>
-            ))}
+                </>
+                )
+            })}
         </div>
         </>
     )
